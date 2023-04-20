ed0f9b65026871f140ff152f1a0ef8b28c26390f
Matshhi <melkgosana@gmail.com>
2023-04-20 14:26:27 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
-	/* 4-puts.c: This is my first C program */
-	#include <stdio.h>
-	puts() {
-		const char* message = "Programming is like building a multilingual puzzle"\n;
-		write(STDOUT_FILENO, message, sizeof(message)-1);
-		return 0;
-	}
+#include <stdio.h>
+main() 
+{
+putchar("Programming is like building a multilingual puzzle");
+return 0
+}
+
