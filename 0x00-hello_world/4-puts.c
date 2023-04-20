3c0a57b93032074e869c4c60375bc203ac3de2bb
Matshhi <melkgosana@gmail.com>
2023-04-20 15:14:43 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
-main() 
+/**
+* main -  a C program that prints exactly "Programming is like building a multilingual puzzel."
+*Return: 0 always
+*/
+int main(void)
 {
-putchar("Programming is like building a multilingual puzzle");
-return 0
-}
+puts("\"Programming is like building a multilingual puzzle\n");
 
+return(0);
+}
